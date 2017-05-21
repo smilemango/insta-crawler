2212d7cf8c4aa8aaf441373cdcb3abe1c77c96ea
smilemango <smileman.god@gmail.com>
2017-05-21 13:40:47 +0900

    팔로워 리스트를 가져오는데 성공함

--- insta_crawler.py
+++ insta_crawler.py
@@ -1,52 +1,128 @@
-import http.client, urllib.parse
+import requests
+import json
+import pprint
+import re
 
-# Host: www.instagram.com
-# User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0
-# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-# Accept-Language: en-US,en;q=0.5
-# Accept-Encoding: gzip, deflate, br
-# Upgrade-Insecure-Requests: 1
-# Cookie: mid=WRq09gAEAAF1Ypa2afCJ7qC1hDoy; s_network=""; csrftoken=u3kZdgBObVVM4cMBmIilGt70fjkihtVd
-# Connection: keep-alive
 
-headers = {"Host": "www.instagram.com",
+class InstaCrawler:
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+        self._session = requests.session()
+        self._is_logged_in = False
+
+        headers = {
+            "Host": "www.instagram.com",
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
             "Accept-Encoding": "gzip, deflate, br",
-            "Upgrade-Insecure-Requests":"1",
             "Connection": "keep-alive"
-            }
+        }
 
-conn = http.client.HTTPSConnection("www.instagram.com")
-conn.request("GET","/")
-response = conn.getresponse()
-data = response.read()
-print(data)
-conn.close()
+        r = self._session.get('http://www.instagram.com', headers=headers)
+        self._last_cookies = r.cookies
 
 
-#
-#
-# params = urllib.parse.urlencode({'username': 'smileman_god@naver.com', 'password': 'xxxxxx'})
-#
-#
-# headers = {"Host": "www.instagram.com",
-#             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0",
-#             "Accept": "*/*",
-#             "Accept-Language": "en-US,en;q=0.5",
-#             "Accept-Encoding": "gzip, deflate, br",
-#             "X-Instagram-AJAX": "1",
-#             "Content-Type": "application/x-www-form-urlencoded",
-#             "X-Requested-With": "XMLHttpRequest",
-#             "Referer": "https://www.instagram.com/",
-#             "Connection": "keep-alive",
-#             "X-CSRFToken": "6VEnDTMwKiX3g0vGVvBucJLJ9vaOmfku",
-#             "Cookie": "mid=WRq09gAEAAF1Ypa2afCJ7qC1hDoy; s_network=""; rur=FRC; ig_vw=1920; ig_pr=1; csrftoken=6VEnDTMwKiX3g0vGVvBucJLJ9vaOmfku"
-#             }
-# conn = http.client.HTTPSConnection("www.instagram.com")
-# conn.request("POST", "/accounts/login/ajax/", params, headers)
-# response = conn.getresponse()
-# print(response.status, response.reason)
-# data = response.read()
-# conn.close()
+    def login(self):
+        headers = {
+            "Host": "www.instagram.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "X-CSRFToken": self._last_cookies['csrftoken'],
+            "X-Instagram-AJAX": "1",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": "https://www.instagram.com/",
+        }
+        data = {
+            "username": self._username,
+            "password": self._password
+        }
+
+        r = self._session.post("https://www.instagram.com/accounts/login/ajax/", headers=headers, data=data, cookies=self._last_cookies)
+        self._last_cookies = r.cookies
+
+        result = json.loads(r.text)
+
+        # login failure
+        # {"authenticated": false, "user": true, "status": "ok"}
+        if result['authenticated'] == True:
+            self._is_logged_in = True
+            return True
+        else :
+            return False
+
+
+
+    def logged_in(self):
+        return self._is_logged_in
+
+
+    def get_followed_by(self, id, after = None):
+        if self._is_logged_in == False:
+            return None
+
+        headers = {
+            "Host": "www.instagram.com",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
+            "Accept": "*/*",
+            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://www.instagram.com/jongsuk0206/",
+            "X-Requested-With": "XMLHttpRequest",
+            "Cookie": "csrftoken=ApIx0yUv0cFCirAaJii1zew8TFNfumxg; mid=WR238QAEAAFU70-yHlvVY_5Mk2Rz; ds_user_id=5470068000; sessionid=IGSC4d4e2b7b6137db271677bdf87f70c12d3c774c1447064bcef476c13de862281e%3AiCVSwz0UoYBgp0q6Y6xYdPoty49y3t4n%3A%7B%22_auth_user_id%22%3A5470068000%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_token_ver%22%3A2%2C%22_token%22%3A%225470068000%3AAxUAmzDOScRbtOmpR9Y7IO31MbW92Civ%3A71026bfd5e3ccd44530f18e50901d99a582a4aa2250f0d476a74980ee679ff85%22%2C%22_platform%22%3A4%2C%22last_refreshed%22%3A1495288424.3225357533%2C%22asns%22%3A%7B%22time%22%3A1495288426%2C%22211.218.50.5%22%3A4766%7D%7D; s_network=""; rur=ATN; ig_vw=1440; ig_pr=1",
+            "Connection": "keep-alive",
+            "Cache-Control": "max-age=0",
+            "X-CSRFToken": self._last_cookies['csrftoken']
+        }
+
+        url = "https://www.instagram.com/graphql/query/?query_id=17851374694183129&id=%s&first=20" % id
+
+        if after != None :
+            url = url + "&after" + after
+
+        r = self._session.get(url, headers=headers, cookies=self._last_cookies)
+        self._last_cookies = r.cookies
+
+        result = json.loads(r.text)
+        return result
+
+    def get_id_by_username(self, username):
+
+        url = "https://www.instagram.com/" + username
+        headers = {
+            "Host": "www.instagram.com",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
+            "Accept": "*/*",
+            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://www.instagram.com/jongsuk0206/",
+            "X-Requested-With": "XMLHttpRequest",
+            "Cookie": "csrftoken=ApIx0yUv0cFCirAaJii1zew8TFNfumxg; mid=WR238QAEAAFU70-yHlvVY_5Mk2Rz; ds_user_id=5470068000; sessionid=IGSC4d4e2b7b6137db271677bdf87f70c12d3c774c1447064bcef476c13de862281e%3AiCVSwz0UoYBgp0q6Y6xYdPoty49y3t4n%3A%7B%22_auth_user_id%22%3A5470068000%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_token_ver%22%3A2%2C%22_token%22%3A%225470068000%3AAxUAmzDOScRbtOmpR9Y7IO31MbW92Civ%3A71026bfd5e3ccd44530f18e50901d99a582a4aa2250f0d476a74980ee679ff85%22%2C%22_platform%22%3A4%2C%22last_refreshed%22%3A1495288424.3225357533%2C%22asns%22%3A%7B%22time%22%3A1495288426%2C%22211.218.50.5%22%3A4766%7D%7D; s_network=""; rur=ATN; ig_vw=1440; ig_pr=1",
+            "Connection": "keep-alive",
+            "Cache-Control": "max-age=0",
+            "X-CSRFToken": self._last_cookies['csrftoken']
+        }
+        r = self._session.get(url,headers=headers, cookies = self._last_cookies)
+        self._last_cookies = r.cookies
+
+        m = re.findall('"id": "([0-9]{10})"', r.text)
+        if len(m) > 1 :
+            return m[1]
+        else:
+            return None
+
+
+ic = InstaCrawler("smileman_god@naver.com","xxxxx")
+ic.login()
+print(ic.login())
+id = ic.get_id_by_username("jongsuk0206")
+my_followers = ic.get_followed_by(id)  # "1408289748"
+pprint.pprint(my_followers)
+
+if my_followers["data"]["user"]["edge_followed_by"]["page_info"]["has_next_page"] == True :
+    my_followers2 = ic.get_followed_by("1408289748",after = my_followers["data"]["user"]["edge_followed_by"]["page_info"]["end_cursor"])
+    pprint.pprint(my_followers2)
--- request_test.py
+++ request_test.py
@@ -1,7 +1,106 @@
 import requests
 s = requests.Session()
-r = s.get('http://www.instagram.com')
 
+
+# 1. Access FirstPage
+# - Get 'csrftoken'(important)
+
+headers = {
+    "Host": "www.instagram.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive"
+}
+
+r = s.get('http://www.instagram.com',headers=headers)
+
+print("HEADER==> " , r.headers)
+print("*"*50)
+print(r.cookies)
+
+
+#2. Login
+headers = {
+    "Host": "www.instagram.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "X-CSRFToken": r.cookies['csrftoken'],
+    "X-Instagram-AJAX": "1",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "X-Requested-With": "XMLHttpRequest",
+    "Referer": "https://www.instagram.com/",
+}
+data = {
+    "username":"smileman_god@naver.com",
+    "password":"zaq1235"
+}
+
+r = s.post("https://www.instagram.com/accounts/login/ajax/",headers=headers, data=data ,cookies = r.cookies)
+
+print(r.status_code)
+print("HEADER ===> ", r.headers)
+print("*"*50)
 print(r.text)
+print("*"*50)
 
-s.post
+
+
+#3. Request User Page
+# query_id -> function id
+# id -> 사용자의 unique id
+# 17845312237175864
+# https://www.instagram.com/graphql/query/?query_id=17845312237175864&id=1408289748
+
+# GET 방식으로 요청함
+# Request Header
+# Host: www.instagram.com
+# User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0
+# Accept: */*
+# Accept-Language: ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3
+# Accept-Encoding: gzip, deflate, br
+# Referer: https://www.instagram.com/jongsuk0206/
+# X-Requested-With: XMLHttpRequest
+# Cookie: csrftoken=ApIx0yUv0cFCirAaJii1zew8TFNfumxg; mid=WR238QAEAAFU70-yHlvVY_5Mk2Rz; ds_user_id=5470068000; sessionid=IGSC4d4e2b7b6137db271677bdf87f70c12d3c774c1447064bcef476c13de862281e%3AiCVSwz0UoYBgp0q6Y6xYdPoty49y3t4n%3A%7B%22_auth_user_id%22%3A5470068000%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_token_ver%22%3A2%2C%22_token%22%3A%225470068000%3AAxUAmzDOScRbtOmpR9Y7IO31MbW92Civ%3A71026bfd5e3ccd44530f18e50901d99a582a4aa2250f0d476a74980ee679ff85%22%2C%22_platform%22%3A4%2C%22last_refreshed%22%3A1495288424.3225357533%2C%22asns%22%3A%7B%22time%22%3A1495288426%2C%22211.218.50.5%22%3A4766%7D%7D; s_network=""; rur=ATN; ig_vw=1440; ig_pr=1
+# Connection: keep-alive
+# Cache-Control: max-age=0
+
+headers = {
+    "Host": "www.instagram.com",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Accept": "*/*",
+    "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.instagram.com/jongsuk0206/",
+    "X-Requested-With": "XMLHttpRequest",
+    "Cookie": "csrftoken=ApIx0yUv0cFCirAaJii1zew8TFNfumxg; mid=WR238QAEAAFU70-yHlvVY_5Mk2Rz; ds_user_id=5470068000; sessionid=IGSC4d4e2b7b6137db271677bdf87f70c12d3c774c1447064bcef476c13de862281e%3AiCVSwz0UoYBgp0q6Y6xYdPoty49y3t4n%3A%7B%22_auth_user_id%22%3A5470068000%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_token_ver%22%3A2%2C%22_token%22%3A%225470068000%3AAxUAmzDOScRbtOmpR9Y7IO31MbW92Civ%3A71026bfd5e3ccd44530f18e50901d99a582a4aa2250f0d476a74980ee679ff85%22%2C%22_platform%22%3A4%2C%22last_refreshed%22%3A1495288424.3225357533%2C%22asns%22%3A%7B%22time%22%3A1495288426%2C%22211.218.50.5%22%3A4766%7D%7D; s_network=""; rur=ATN; ig_vw=1440; ig_pr=1",
+    "Connection": "keep-alive",
+    "Cache-Control": "max-age=0",
+    "X-CSRFToken": r.cookies['csrftoken']
+}
+
+# Content-Type: application/json
+# Vary: Cookie, Accept-Language, Accept-Encoding
+# Cache-Control: private, no-cache, no-store, must-revalidate
+# Pragma: no-cache
+# Expires: Sat, 01 Jan 2000 00:00:00 GMT
+# Content-Language: ko
+# Content-Length: 4659
+# Content-Encoding: gzip
+# Date: Sat, 20 May 2017 14:28:33 GMT
+# Strict-Transport-Security: max-age=86400
+# Set-Cookie: csrftoken=ApIx0yUv0cFCirAaJii1zew8TFNfumxg; expires=Sat, 19-May-2018 14:28:33 GMT; Max-Age=31449600; Path=/; Secure
+# rur=ATN; Path=/
+# ds_user_id=5470068000; expires=Fri, 18-Aug-2017 14:28:33 GMT; Max-Age=7776000; Path=/
+# X-Firefox-Spdy: h2
+
+function_url = "https://www.instagram.com/graphql/query/?query_id=17845312237175864&id=1408289748"
+r = s.get(function_url,headers = headers, cookies=r.cookies)
+print(r.status_code)
+print("HEADER ===> ", r.headers)
+print("*"*50)
+print(r.text)
+print("*"*50)
