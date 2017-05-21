bd1cdaa47b9fe358ef5f0d667e8185cfd33df80a
smilemango <smileman.god@gmail.com>
2017-05-21 23:58:04 +0900

    downloading follows list

---
+++ download_followers.py
@@ -0,0 +1,81 @@
+import insta_crawler
+import pprint
+import os
+import json
+
+DATA_DIR = "./data"
+USER_DICT = []
+
+
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
+
+def download_follows_by_username(username):
+    if ic.is_logged_in() == True:
+        id = ic.get_id_by_username(username)
+        download_follows_by_id(id,username)
+        return True
+    else :
+        return False
+
+
+def download_follows_by_id(id, username = None):
+    # id 타겟 dir이 없으면 만든다.
+    dir4id = DATA_DIR + "/" + id
+    if not os.path.isdir(dir4id):
+        os.mkdir(dir4id)
+
+    # 디렉토리 내에 'END'라는 파일이 있으면 패스함
+    if os.path.exists(dir4id+"/END"):
+        return
+
+    json_follows = ic.get_follows_by_id(id)  # "1408289748"
+    # pprint.pprint(json_follows)
+    # json_follows['data']['user']['edge_follow']['page_info']['end_cursor']
+    # json_follows['data']['user']['edge_follow']['page_info']['has_next_page']
+    #
+    file_idx = 0
+
+    while json_follows['data']['user']['edge_follow']['page_info']['has_next_page'] == True:
+        file_idx = file_idx + 1
+        file_name = "%08d_follows_%s_%s.json" % (file_idx, id, username)
+
+        if file_idx > 1:
+            json_follows = ic.get_follows_by_id(id, after=json_follows['data']['user']['edge_follow']['page_info'][
+                'end_cursor'])
+
+        for node in  json_follows['data']['user']['edge_follow']['edges']:
+            f_id = node['node']['id']
+            f_username = node['node']['username']
+            USER_DICT.append({"id":f_id, "username":f_username})
+
+        outfile_name = dir4id + "/" + file_name
+        print("Writing... '%s'" % outfile_name)
+        with open(outfile_name, 'w') as outfile:
+            json.dump(json_follows, outfile, indent=4)
+
+    touch(dir4id+"/END")
+
+
+if __name__ == "__main__":
+    if os.path.isdir(DATA_DIR) == True :
+        print("'%s' directory already exists." % DATA_DIR)
+    else:
+        os.mkdir(DATA_DIR)
+        print("'%s' directory is made." % DATA_DIR)
+
+    ic = insta_crawler.InstaCrawler("smileman_god@naver.com","my password")
+    ic.login()
+    print(ic.is_logged_in())
+
+    download_follows_by_username("lovelymrsyi")
+    input("Press any key...")
+
+
+    for user in USER_DICT:
+        print("Downloading... '%s'" % user['username'])
+        download_follows_by_id(user["id"], username=user['username'])
+        print("[OK]")
+        input("Press any key...")
--- insta_crawler.py
+++ insta_crawler.py
@@ -57,11 +57,11 @@ class InstaCrawler:
 
 
 
-    def logged_in(self):
+    def is_logged_in(self):
         return self._is_logged_in
 
 
-    def get_followed_by(self, id, after = None):
+    def get_followed_by_id(self, id, after = None):
         if self._is_logged_in == False:
             return None
 
@@ -90,6 +90,35 @@ class InstaCrawler:
         result = json.loads(r.text)
         return result
 
+    def get_follows_by_id(self, id, after=None):
+        if self._is_logged_in == False:
+            return None
+
+        headers = {
+            "Host": "www.instagram.com",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
+            "Accept": "*/*",
+            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://www.instagram.com/jongsuk0206/",
+            "X-Requested-With": "XMLHttpRequest",
+            "Cookie": "csrftoken=ApIx0yUv0cFCirAaJii1zew8TFNfumxg; mid=WR238QAEAAFU70-yHlvVY_5Mk2Rz; ds_user_id=5470068000; sessionid=IGSC4d4e2b7b6137db271677bdf87f70c12d3c774c1447064bcef476c13de862281e%3AiCVSwz0UoYBgp0q6Y6xYdPoty49y3t4n%3A%7B%22_auth_user_id%22%3A5470068000%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_token_ver%22%3A2%2C%22_token%22%3A%225470068000%3AAxUAmzDOScRbtOmpR9Y7IO31MbW92Civ%3A71026bfd5e3ccd44530f18e50901d99a582a4aa2250f0d476a74980ee679ff85%22%2C%22_platform%22%3A4%2C%22last_refreshed%22%3A1495288424.3225357533%2C%22asns%22%3A%7B%22time%22%3A1495288426%2C%22211.218.50.5%22%3A4766%7D%7D; s_network=""; rur=ATN; ig_vw=1440; ig_pr=1",
+            "Connection": "keep-alive",
+            "Cache-Control": "max-age=0",
+            "X-CSRFToken": self._last_cookies['csrftoken']
+        }
+
+        url = "https://www.instagram.com/graphql/query/?query_id=17874545323001329&id=%s&first=20" % id
+
+        if after != None :
+            url = url + "&after=" + after
+
+        r = self._session.get(url, headers=headers, cookies=self._last_cookies)
+        self._last_cookies = r.cookies
+
+        result = json.loads(r.text)
+        return result
+
     def get_id_by_username(self, username):
 
         url = "https://www.instagram.com/" + username
@@ -109,20 +138,10 @@ class InstaCrawler:
         r = self._session.get(url,headers=headers, cookies = self._last_cookies)
         self._last_cookies = r.cookies
 
-        m = re.findall('"id": "([0-9]{10})"', r.text)
+        m = re.findall('"id": "([0-9]{9,10})"', r.text)
         if len(m) > 1 :
             return m[1]
         else:
             return None
 
 
-ic = InstaCrawler("smileman_god@naver.com","xxxxx")
-ic.login()
-print(ic.login())
-id = ic.get_id_by_username("jongsuk0206")
-my_followers = ic.get_followed_by(id)  # "1408289748"
-pprint.pprint(my_followers)
-
-if my_followers["data"]["user"]["edge_followed_by"]["page_info"]["has_next_page"] == True :
-    my_followers2 = ic.get_followed_by("1408289748",after = my_followers["data"]["user"]["edge_followed_by"]["page_info"]["end_cursor"])
-    pprint.pprint(my_followers2)
